cb2f980ca1c659aad88c1d5ec5353cad5228ba24
Loran Briggs <loranbriggs@gmail.com>
2014-01-26 00:28:18 -0800

    edit deploy script

--- deploy-master.sh
+++ deploy-master.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+echo "Deploying master branch at $(date)" >> ~/.forever/Ov-w.log
+
 git pull
 
 forever restart Ov-w
-
-echo "Deployed master branch at $(date)" >> ~/.forever/Ov-w.log
