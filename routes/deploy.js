dd9b2a7a6ca4c5e1242c1ec39dc3edc6a1333c2f
Loran Briggs <loranbriggs@gmail.com>
2014-01-25 16:36:44 -0800

    deploy.js

--- routes/deploy.js
+++ routes/deploy.js
@@ -3,7 +3,7 @@
  */
 
 exports.index = function(req, res){
-  var params = JSON.parse(req.param('payload'));
+  var params = JSON.parse(req.query('payload'));
   if (params.ref == "refs/heads/master") {
     d = new Date();
     console.log("pushed from master at:" + d );
