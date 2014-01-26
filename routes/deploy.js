b2774ec5a3e2d90a870259e8bfa53ed8549efd10
Loran Briggs <loranbriggs@gmail.com>
2014-01-25 16:34:26 -0800

    edites to deploy.js

--- routes/deploy.js
+++ routes/deploy.js
@@ -1,12 +1,10 @@
-var url = require('url');
 /*
  * POST deploy.
  */
 
 exports.index = function(req, res){
-  var parts = url.parse(req.url, true);
-  var params = JSON.parse(parts.query);
-  if (params.payload.ref == "refs/heads/master") {
+  var params = JSON.parse(req.param('payload'));
+  if (params.ref == "refs/heads/master") {
     d = new Date();
     console.log("pushed from master at:" + d );
 
