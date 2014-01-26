8f63f46619902777cfd4badd3c42164e14e6c993
Loran Briggs <loranbriggs@gmail.com>
2014-01-25 17:39:40 -0800

    sdfa

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.configure(function(){
   app.set('view engine', 'ejs');
   app.use(express.favicon());
   app.use(express.logger('dev'));
-  app.use(express.urlencoded());
+  app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
