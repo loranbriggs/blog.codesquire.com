eb754c83c20632c7fb86399d8ac7d51781371199
Loran Briggs <loranbriggs@gmail.com>
2014-01-25 16:06:55 -0800

    update deploy test

--- routes/deploy.js
+++ routes/deploy.js
@@ -4,8 +4,10 @@
  */
 
 exports.index = function(req, res){
-  if (req.body.ref = "refs/heads/master") {
-    console.log("pushed from master");
+  if (req.body.ref == "refs/heads/master") {
+    d = new Date();
+    console.log("pushed from master at:" + d );
+
   } else {
     console.log("pushed not from master");
   }
