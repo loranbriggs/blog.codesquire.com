febf8085fbf6ea8ec7f13fb7445a066b7c98f4a0
Loran Briggs <loranbriggs@gmail.com>
2014-01-25 17:35:24 -0800

    dsfa

--- routes/deploy.js
+++ routes/deploy.js
@@ -3,7 +3,7 @@
  */
 
 exports.index = function(req, res){
-  var payload = req.payload;
+  var payload = req.body.payload;
   console.log(payload);
   //var params = JSON.parse(req.query.payload);
   /*
